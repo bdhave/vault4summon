845ed9e00685f0eb8f294f9525a7e80c3d2f02db
haveber <bernard.dhave@gmail.com>
2021-02-11 17:51:22 +0100

    initialization of Vault transit finished

--- vaultserver/volumes/transit/config/configVaultTransit.sh
+++ vaultserver/volumes/transit/config/configVaultTransit.sh
@@ -1,4 +1,8 @@
-vault audit enable file file_path=vault/logs/audit/audit.log
+#export ROOT4VAULT=vaultserver/volumes/transit
+export ROOT4VAULT=/vault
+
+vault audit enable file file_path=$ROOT4VAULT/logs/audit/audit.log
 vault secrets enable transit
 vault write -f transit/keys/autounseal
-vault policy write/vault/config/autounseal autounseal.hcl
+vault policy write transit-policy $ROOT4VAULT/config/autounseal.hcl
+
