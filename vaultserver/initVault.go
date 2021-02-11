5fa529fd7378c8e02da48e44ebec11b289d71e1e
haveber <bernard.dhave@gmail.com>
2021-02-11 17:51:22 +0100

    initialization of Vault transit finished

--- vaultserver/initVault.go
+++ vaultserver/initVault.go
@@ -3,12 +3,15 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+const sealedVaultStatusCommandExitCode = 2
 
 type status struct {
 	Initialized bool   `json:"initialized"`
@@ -42,19 +45,19 @@ func main() {
 	}
 
 	if status.Sealed {
-		exitIfError(fmt.Errorf("cannot unseal vault"), nil)
+		exitIfError(fmt.Errorf("cannot unseal vault"))
 	}
 }
 
 func getStatus() *status {
-	exitCodes := append(make([]int, 3), 0, 1, 2)
+	exitCodes := append(make([]int, 1), sealedVaultStatusCommandExitCode)
 	jsonData := execute("vault",
 		exitCodes,
 		"status", "-format", "json")
 
 	status := &status{}
 	err := json.Unmarshal(jsonData, status)
-	exitIfError(err, nil)
+	exitIfError(err)
 
 	return status
 }
@@ -66,15 +69,15 @@ func doInit() *initialization {
 	var initialization = &initialization{}
 	err := json.Unmarshal(jsonData, initialization)
 	if err != nil || len(initialization.Seals) < 1 {
-		exitIfError(fmt.Errorf("no seals available"), nil)
+		exitIfError(fmt.Errorf("no seals available"))
 	}
 
 	jsonData, err = json.Marshal(initialization)
-	exitIfError(err, nil)
+	exitIfError(err)
 	//enc := json.NewEncoder(os.Stdout)
 
 	err = ioutil.WriteFile(filename, jsonData, 0644)
-	exitIfError(err, nil)
+	exitIfError(err)
 
 	return initialization
 }
@@ -83,9 +86,9 @@ func doUnseal(init *initialization) *status {
 
 	if init == nil {
 		dat, err := ioutil.ReadFile(filename)
-		exitIfError(err, nil)
+		exitIfError(err)
 		err = json.Unmarshal(dat, init)
-		exitIfError(err, nil)
+		exitIfError(err)
 	}
 
 	if getStatus().Sealed && init != nil && init.Seals != nil {
@@ -113,7 +116,7 @@ func doOneUnseal(value string) *seal {
 func execute(command string, ignoredExitCode []int, args ...string) []byte {
 	var err error
 	command, err = exec.LookPath(command)
-	exitIfError(err, nil)
+	exitIfError(err)
 
 	cmd := exec.Command(command, args...)
 	cmd.Env = os.Environ()
@@ -121,43 +124,87 @@ func execute(command string, ignoredExitCode []int, args ...string) []byte {
 
 	out, err := cmd.Output()
 	if err != nil {
-		exitError := err.(*exec.ExitError)
-		exitCode := exitError.ExitCode()
-		if ignoredExitCode != nil {
-			for i := 0; i < len(ignoredExitCode); i++ {
-				if ignoredExitCode[i] == exitCode {
-					return out
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			exitCode := exitError.ExitCode()
+			if ignoredExitCode != nil {
+				for i := 0; i < len(ignoredExitCode); i++ {
+					if ignoredExitCode[i] == exitCode {
+						return out
+					}
 				}
 			}
 		}
-		if exitCode == 1 {
 
-		}
-		if exitCode == 2 {
-
-		}
-		/*
-			Local errors such as incorrect flags, failed validations, or wrong numbers of arguments return an exit code of 1.
-			Any remote errors such as API failures, bad TLS, or incorrect API parameters return an exit status of 2
-		*/
-		exitIfError(err, exitError.Stderr)
+		exitIfError(newCommandError(exitError))
 	}
 
 	return out
 }
 
-func exitIfError(err error, errOutput []byte) {
+func exitIfError(err error) {
 	if err == nil {
 		return
 	}
-	sliceOutputStream(errOutput)
-	_, _ = os.Stderr.Write([]byte(err.Error()))
-	os.Exit(1)
-}
 
+	var exitCode int = 1
+	var exitError *exec.ExitError
+	if errors.As(err, &exitError) {
+		exitCode = exitError.ExitCode()
+		_ = sliceOutputStream(exitError.Stderr)
+		_, _ = os.Stderr.Write([]byte(err.Error()))
+	}
+	os.Exit(exitCode)
+
+}
 func sliceOutputStream(out []byte) []string {
 	if out == nil {
 		return nil
 	}
 	return strings.Split(string(out), "\n")
 }
+
+type CommandError struct {
+	err *exec.ExitError
+}
+
+func (e *CommandError) Error() string {
+	exitCode := e.err.ExitCode()
+	if exitCode == 1 {
+		// Local errors such as incorrect flags, failed validations, or wrong numbers of arguments
+	}
+	if exitCode == 2 {
+		// Any remote errors such as API failures, bad TLS, or incorrect API parameters
+	}
+
+	return ": " + e.err.Error()
+
+}
+
+func newCommandError(err *exec.ExitError) error {
+	if err == nil {
+		// As a convenience, if err is nil, NewSyscallError returns nil.
+		return nil
+	}
+	return &CommandError{err}
+}
+
+/*
+
+type WrappedError struct {
+    Context string
+    Err     error
+}
+
+func (w *WrappedError) Error() string {
+    return fmt.Sprintf("%s: %v", w.Context, w.Err)
+}
+
+func Wrap(err error, info string) *WrappedError {
+    return &WrappedError{
+        Context: info,
+        Err:     err,
+    }
+}
+
+*/
