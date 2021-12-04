77b1ee7c207db4df1028dfcf44ee7765815da5d7
haveber <bernard.dhave@gmail.com>
2021-12-04 18:27:38 +0100

    remove startSummonContainer.sh scripts

--- startSummonContainer.sh
+++
@@ -1,7 +0,0 @@
-docker build -f dockerfile -t summon .
-
-docker run -it --rm \
-    --env VAULT_TOKEN=myRootToken \
-    --env VAULT_ADDR=http://vault:8200 \
-    --link vault \
-    --name summon summon
